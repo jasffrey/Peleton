2a5d5342c4dab058dca708214e89fc65d3df3a03
Jason Chen <jasonchenubc@gmail.com>
2023-07-14 14:23:22 -0700

    main added

---
+++ src/peloton/Main.java
@@ -0,0 +1,20 @@
+package peloton;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import peloton.datamanagement.*;
+import peloton.logging.*;
+import peloton.processor.*;
+import peloton.ui.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
